4210601dd77a421b012c404547712caa705e1476
saul <saul@saul-desktop.(none)>
2015-05-06 16:41:16 -0400

    Posts ordenados por fecha de envío.

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -6,9 +6,15 @@ Template.postSubmit.events({
 			url: $(e.target).find('[name=url]').val(),
 			title: $(e.target).find('[name=title]').val()
 		};
+
 		Meteor.call('postInsert',post,function(error,result){
 			if(error)
 				return alert(error.reason);
+
+		// show this result but route anyway
+      	if (result.postExists)
+       		alert('This link has already been posted');	
+
 			Router.go('postPage',{_id:result._id});
 		});
 	}
